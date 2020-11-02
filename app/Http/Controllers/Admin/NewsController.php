270de426c69d51b94dcac9f0b4912d2ddf69b780
”takemurataiki” <>
2020-11-02 02:13:46 +0000

    laravel14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -15,38 +15,16 @@ class NewsController extends Controller
       return view('admin.news.create');
       //view(‘admin.news.create’); = admin/newsディレクトリ配下のcreate.blade.php というファイルを呼び出す という意味
   }
-
-
-
-public function create(Request $request)
+  
+  public function create(Request $request)
   {
-
-      // 以下を追記
-      // Varidationを行う
-      $this->validate($request, News::$rules);
-
-      $news = new News;
-      $form = $request->all();
-
-      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-      if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
-      } else {
-          $news->image_path = null;
-      }
-
-      // フォームから送信されてきた_tokenを削除する
-      unset($form['_token']);
-      // フォームから送信されてきたimageを削除する
-      unset($form['image']);
-
-      // データベースに保存する
-      $news->fill($form);
-      $news->save();
-
+      // admin/news/createにリダイレクトする
       return redirect('admin/news/create');
-  }
+  }  
+
+
+
+
   
   public function index(Request $request)
   {
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -6,7 +6,38 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
+                <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                   
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-10">
+                            <input type="name" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別</label>
+                        <div class="col-md-10">
+                            <input type="gender" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        <div class="col-md-10">
+                            <input type="hobby" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
